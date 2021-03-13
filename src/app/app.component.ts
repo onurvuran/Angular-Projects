3067aa4c9106aa128f120c627f578e6967a68e81
ONUR <ONUR@DESKTOP-36KJSTU>
2021-03-12 23:18:59 -0900

    12 mart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AIA';
+  products =[{product:1,productName:'Glass',categoryId:1,unitPrice:5},
+  {product:2,productName:'Book',categoryId:1,unitPrice:5},
+  {product:3,productName:'Chair',categoryId:1,unitPrice:5},
+  {product:4,productName:'Angular',categoryId:1,unitPrice:5}]
 }
